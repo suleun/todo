313959a7d23d137c04b066a21b106241f86bbccd
suleun <teaaa030@gmail.com>
2021-10-18 16:32:41 +0900

    The list of things to do, complete, and full clicks should look different, but this is not the case.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+    <div id="app">
+        <div class="container">
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+            <h1 class="text-center">
+                Todo
+            </h1>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+            <input
+                type="text"
+                class="form-control mb-4"
+                v-model="userInput"
+                @keyup.enter="addNewTodo">
+                <div class="list-group mb-4">
+                    <template>
+
+                        <button
+                            class="list-group-item"
+                            v-for="todo in activeTodoList"
+                            @click="toggleTodoState(todo)"
+                            v-bind:key="todo">
+                            {{ todo.label }}
+                        </button>
+
+                    </template>
+
+                </div>
+
+                <div class="text-right">
+
+                    <button type="button" class="btn btn-sm" @click="changeCurrentState('active')">
+                        할일
+                    </button>
+                    <button type="button" class="btn btn-sm" @click="changeCurrentState('done')">
+                        완료
+                    </button>
+                    <button type="button" class="btn btn-sm" @click="changeCurrentState('all')">
+                        전체
+                    </button>
+
+                </div>
+
+            </div>
+        </div>
+    </template>
+
+    <script>
+        export default {
+            name: 'app',
+
+            data() {
+                return {userInput: '', todoList: [], currentState: 'active'};
+            },
+
+            methods: {
+                addNewTodo() {
+                    this
+                        .todoList
+                        .push({label: this.userInput, state: 'active'});
+                    this.userInput = '';
+                },
+
+                toggleTodoState(todo) {
+                    todo.state = todo.state === 'active'
+                        ? 'done'
+                        : 'active';
+                }
+            },
+
+            computed: {
+                activeTodoList() {
+                    return this
+                        .todoList
+                        .filter(
+                            todo => this.currentState === 'all' || todo.state === this.currentState
+                        );
+                }
+            },
+
+            changeCurrentState(state) {
+                this.currentState = state;
+            },
+
+            components: {}
+        }
+    </script>
+
+    <style>
+        #app {
+            font-family: Avenir, Helvetica, Arial, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            text-align: center;
+            color: #2c3e50;
+            margin-top: 60px;
+        }
+    </style>
